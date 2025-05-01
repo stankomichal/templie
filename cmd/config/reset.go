faeb30239ac1a3ee58de55b92c9390adc5f0530b
Michal Stanko <michal.stankoml@gmail.com>
2025-05-01 13:31:25 +0200

    Add verbose to a reset command.

--- cmd/config/reset.go
+++ cmd/config/reset.go
@@ -4,9 +4,10 @@ Copyright © 2025 Michal Stanko michal.stankoml@gmail.com
 package config
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/stankomichal/templie/internal/config"
+	"github.com/stankomichal/templie/internal/contextKey"
+	"github.com/stankomichal/templie/internal/helpers"
 )
 
 // resetCmd represents the reset command
@@ -24,13 +25,21 @@ Examples:
 
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		config := cmd.Context().Value("config").(*config.Config)
-		varValue, err := config.Reset(args[0])
+		ctx := cmd.Context()
+		cfg := ctx.Value(contextKey.ConfigKey).(*config.Config)
+
+		helpers.VerbosePrintln(ctx, "Starting config reset process")
+		helpers.VerbosePrintf(ctx, "Variable name: %s\n", args[0])
+
+		helpers.VerbosePrintf(ctx, "Resetting variable %s to default value\n", args[0])
+		varValue, err := cfg.Reset(args[0])
 		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(varValue)
+			cmd.PrintErrf("Error resetting variable: %v\n", err)
+			return
 		}
+		cmd.Printf("Variable reset successfully to: %s\n", varValue)
+
+		helpers.VerbosePrintln(ctx, "Config reset process completed")
 	},
 }
 
